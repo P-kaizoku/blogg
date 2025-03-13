e32089f7b82889694932509cef5aabe50af3cd21
pikachu <pabitram322@gmail.com>
2025-03-13 16:58:42 +0530

    Configure CORS to allow requests from the production frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,15 +10,17 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://blogg-dun-nine.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
